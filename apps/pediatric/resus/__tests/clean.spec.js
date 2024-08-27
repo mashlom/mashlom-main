33e76651fba1e2b801037f692ed5f74489d366e0
Tal Avinari <talavinari@gmail.com>
2024-08-27 18:49:27 +0300

    start generify drug test cases

--- apps/pediatric/resus/__tests/clean.spec.js
+++ apps/pediatric/resus/__tests/clean.spec.js
@@ -3,6 +3,7 @@ require('angular-mocks');
 require('../js/resus'); // Require your AngularJS module
 angular.module('ngAnimate', []); // Mocking ngAnimate
 angular.module('common-directives', []); // Mocking common-directives
+const fs = require('fs');
 
 describe('ResusController', () => {
     let $controller;
@@ -13,21 +14,43 @@ describe('ResusController', () => {
         $controller = _$controller_;
     }));
 
+    const drugMap = loadAndProcessJSON('./apps/pediatric/resus/__tests/resus-drugs-definitions-duplicate-testing.json');
+
+    function getDrugData(name, howToGive, dose_per_kg) {
+        // Create the key from the input parameters
+        const key = `${name}|${howToGive}|${dose_per_kg}`;
+        return drugMap.get(key) || null;  // Return null if the key doesn't exist
+    }
+
     it('should find administer function', () => {
         const $scope = {};
         const controller = $controller('ResusController', { $scope });
         controller.weight = 5;
         
-        const result = controller.calcAmountToAdminister({
-            "name": "Adrenaline 1:10,000",
-            "howToGive": "מתן בפוש רק בהחייאה IV",
-            "dose_per_kg": 0.01,
-            "dose_unit": "mg",
-            "concentration": "1/10",
-            "maxDose": "1",
-            "maxDoseUnit": "mg",
-            "prepare_instructions": "To get Adrenaline 1mg/10ml (1:10,000) take 1 ml of Adrenaline 1mg/1ml and dilute it  with 9 ml of NACL 0.9%"
-         });
+        let result = controller.calcAmountToAdminister(getDrugData("Adrenaline 1:10,000", "מתן בפוש רק בהחייאה IV", 0.01));
         expect(result).toBe('0.5');
+        
+        result = controller.calcAmountToAdminister(getDrugData("Atropine 1:10,000", "IV", 0.02));
+        expect(result).toBe('1');
     });
+
+    function loadAndProcessJSON(filePath) {
+        // Read the JSON file
+        const data = fs.readFileSync(filePath, 'utf8');
+        const jsonData = JSON.parse(data);
+    
+        // Create a hash map
+        const hashMap = new Map();
+    
+        // Populate the hash map
+        jsonData.sections.forEach(section => {
+            section.drugs.forEach(drug => {
+                // Create the key from name, howToGive, and dose_per_kg
+                const key = `${drug.name}|${drug.howToGive}|${drug.dose_per_kg}`;
+                hashMap.set(key, drug);
+            });
+        });
+    
+        return hashMap;
+    }
 });
