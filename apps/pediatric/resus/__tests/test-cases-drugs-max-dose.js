f4e1072401c1ad3209f4cffcf129bc216c4ead29
Tal Avinari <talavinari@gmail.com>
2024-09-13 15:25:41 +0300

    fix name for fluids

--- apps/pediatric/resus/__tests/test-cases-drugs-max-dose.js
+++ apps/pediatric/resus/__tests/test-cases-drugs-max-dose.js
@@ -9,7 +9,7 @@ module.exports = [
     { childWeight: 101, drugName: 'Midazolam', drugType: 'IV', dosage: 0.1, expectedResult: { totalDose: 10, volume: 10 } },
     { childWeight: 151, drugName: 'Suxamethonium', drugType: 'IV', dosage: 1, expectedResult: { totalDose: 150, volume: 15 } },
     { childWeight: 51, drugName: 'Adrenaline 1:1000', drugType: 'IM', dosage: 0.01, expectedResult: { totalDose: 0.5, volume: 0.5 } },
-    { childWeight: 51, drugName: 'Fluid Bolus', drugType: 'IV', dosage: 20, expectedResult: { totalDose: 1000, volume: 1000 } },
+    { childWeight: 51, drugName: 'Fluid Bolus NACL 0.9%', drugType: 'IV', dosage: 20, expectedResult: { totalDose: 1000, volume: 1000 } },
     { childWeight: 61, drugName: 'Adenosine 1st', drugType: 'IV', dosage: 0.1, expectedResult: { totalDose: 6, volume: 2 } },
     { childWeight: 61, drugName: 'Adenosine 2nd', drugType: 'IV', dosage: 0.2, expectedResult: { totalDose: 12, volume: 4 } },
     { childWeight: 26, drugName: 'Hydrocortisone', drugType: 'IV', dosage: 4, expectedResult: { totalDose: 100, volume: null } },
