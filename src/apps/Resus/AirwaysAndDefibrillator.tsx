6f8840f5d9769c8b8d555952a3b18d9aaa0113b8
Yonatan Katz <katzyonatan@gmail.com>
2024-10-10 11:19:16 +0300

    fix defi padding

--- src/apps/Resus/AirwaysAndDefibrillator.tsx
+++ src/apps/Resus/AirwaysAndDefibrillator.tsx
@@ -29,20 +29,20 @@ useEffect(() => {
 
   return (
     <div className="cards-container row row-cols-1 row-cols-md-1 g-4">
-      <div className="col">
+      <div className="col" style={{ padding: "0px"}}>
         <div className="card">
           <div className="card-body row">
-            <div className="col-md-6" style={{ paddingTop: '10px' }}>
+            <div className="col-md-6" >
               <h5 className="card-title" style={{ fontWeight: 'bold' }}>Airways</h5>
-              <ul className="list-group" style={{ backgroundColor: '#B9EDE7', borderRadius: '10px', padding: '15px', lineHeight: '1rem' }}>
+              <ul className="list-group" style={{ backgroundColor: '#B9EDE7', borderRadius: '10px', padding: '15px', marginRight: "15px", lineHeight: '1rem' }}>
                 <span className="defi-data">Laryngiscope blade: {airwaysForAge.blade}</span><br />
                 <span className="defi-data">ETT diameter (Cuffed): {airwaysForAge.cuffedETT}</span><br />
                 <span className="defi-data">LMA Size: {airwaysForAge.lma}</span>
               </ul>
             </div>
-            <div className="col-md-6" style={{ paddingTop: '10px' }}>
+            <div className="col-md-6" style={{  }}>
               <h5 className="card-title" style={{ fontWeight: 'bold' }}>Defibrillator</h5>
-              <ul className="list-group" style={{ backgroundColor: '#B9EDE7', borderRadius: '10px', padding: '15px', lineHeight: '1rem' }}>
+              <ul className="list-group" style={{ backgroundColor: '#B9EDE7', borderRadius: '10px', padding: '15px', marginRight: "15px",  lineHeight: '1rem' }}>
                 <span className="defi-data">{getDefi(2)} (2J/Kg)</span><br />
                 <span className="defi-data">{getDefi(4)} (4J/Kg)</span>
               </ul>
